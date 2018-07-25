191f0572493ebbb9e4efc91eee414896374f284c
xiehaijun <zhenxindefeng@126.com>
2018-07-25 17:58:56 +0800

    自动生成tk代码

---
+++ spring-shiro/src/test/java/org/haijun/study/dao/TkTestMapper.java
@@ -0,0 +1,7 @@
+package org.haijun.study.dao;
+
+import org.haijun.study.model.TkTest;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TkTestMapper extends Mapper<TkTest> {
+}
---
+++ spring-shiro/src/test/java/org/haijun/study/model/TkTest.java
@@ -0,0 +1,103 @@
+package org.haijun.study.model;
+
+import javax.persistence.*;
+
+@Table(name = "`t_test`")
+public class TkTest {
+    /**
+     * 主键
+     */
+    @Column(name = "`id`")
+    @GeneratedValue(generator = "JDBC")
+    private Long id;
+
+    /**
+     * 姓名
+     */
+    @Column(name = "`name`")
+    private String name;
+
+    /**
+     * 邮箱
+     */
+    @Column(name = "`email`")
+    private String email;
+
+    /**
+     * 年龄
+     */
+    @Column(name = "`age`")
+    private Integer age;
+
+    /**
+     * 获取主键
+     *
+     * @return id - 主键
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * 设置主键
+     *
+     * @param id 主键
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取姓名
+     *
+     * @return name - 姓名
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置姓名
+     *
+     * @param name 姓名
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取邮箱
+     *
+     * @return email - 邮箱
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置邮箱
+     *
+     * @param email 邮箱
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取年龄
+     *
+     * @return age - 年龄
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * 设置年龄
+     *
+     * @param age 年龄
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
