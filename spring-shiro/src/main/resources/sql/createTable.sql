f49b805bebb97401580b91d7030814337fb5515f
xiehaijun <zhenxindefeng@126.com>
2018-07-25 17:27:21 +0800

    数据库创建测试

--- spring-shiro/src/main/resources/sql/createTable.sql
+++ spring-shiro/src/main/resources/sql/createTable.sql
@@ -3,11 +3,9 @@ CREATE TABLE `t_test` (
   `name` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
   `age` int(3) NOT NULL COMMENT '年龄'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci;
-
+) ;
 ALTER TABLE `t_test`
   ADD PRIMARY KEY (`id`);
-
 ALTER TABLE `t_test`
   MODIFY `id` bigint(18) NOT NULL AUTO_INCREMENT COMMENT '主键';
 COMMIT;
