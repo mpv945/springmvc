4d07f278285e4ef24c6b4d45c7c3d24dd5553cba
xiehaijun <zhenxindefeng@126.com>
2018-08-17 21:52:55 +0800

    分页

--- spring-cxf/src/main/java/org/haijun/study/lambda/LambdaExample.java
+++ spring-cxf/src/main/java/org/haijun/study/lambda/LambdaExample.java
@@ -31,9 +31,50 @@ public class LambdaExample {
     //items.forEach(item->{if("C".equals(item)){ System.out.println(item); } });
 
     // 排序
-    // userMap.values().stream().sorted(Comparator.comparing(User::getName, UserNameComparator.INSTANCE)).collect(Collectors.toList());
+    //userMap.values().stream().sorted(Comparator.comparing(User::getName, UserNameComparator.INSTANCE)).collect(Collectors.toList());
+    //升序排序，流的写法
+    //list.stream().sorted(Comparator.comparing(str -> str)).collect(Collectors.toList()).forEach(System.out::println);
+    //升序排序，默认写法
+        /*Collections.sort(list);
+        System.out.println("list = " + list);*/
+    //自定义排序器,默认写法
+        /*Collections.sort(list, (item1, item2) -> item1.length() - item2.length()); //升序
+        Collections.sort(list, (item1, item2) -> item2.length() - item1.length());   //降序
+        System.out.println("list = " + list);*/
+    //自定义排序器,Comparator写法
+        /*Collections.sort(list, Comparator.comparingInt(str -> str.length()));                       //升序
+        Collections.sort(list, Comparator.comparingInt(str -> str.length()).reversed());              //降序,会报错，因为编译器在这里无法推断str的类型为String，而是推断出事一个Objcet
+        Collections.sort(list, Comparator.comparingInt((String str) -> str.length()).reversed());*/   //降序,显示的指定一个类型
+    //自定义排序器,Comparator写法2
+        /*Collections.sort(list, Comparator.comparingInt(String::length));           //升序
+        Collections.sort(list, Comparator.comparingInt(String::length).reversed());  //降序
+        System.out.println("list = " + list);*/
+
+    //直接调用list的排序方法，Collections.sort()本质还是调用list.sort方法
+        /*list.sort(Comparator.comparingInt(String::length));             //升序
+        list.sort(Comparator.comparingInt(String::length).reversed());    //降序*/
+    /**两层排序:先按照长度排序，再按照字符串顺序**/
+    //Collections.sort(list, Comparator.comparingInt(String::length).thenComparing(String.CASE_INSENSITIVE_ORDER));   //不区分大小写的排序
+    //Collections.sort(list, Comparator.comparingInt(String::length).thenComparing(String::compareTo));
+    //Collections.sort(list, Comparator.comparingInt(String::length).thenComparing((item1, item2) -> item1.toLowerCase().compareTo(item2.toLowerCase())));
+    //Collections.sort(list, Comparator.comparingInt(String::length).thenComparing(Comparator.comparing(String::toLowerCase)));
+    //Collections.sort(list, Comparator.comparingInt(String::length).thenComparing(Comparator.comparing(String::toLowerCase, Comparator.reverseOrder())));
+
+    //Collections.sort(list, Comparator.comparingInt(String::length).
+    //thenComparing(Comparator.comparing(String::toLowerCase, Comparator.reverseOrder())).
+    //thenComparing(Comparator.reverseOrder()));//和上一个结果是一样的，因为已经排好序了，最后一个就不起作用了
     // 多重排序
     //.sorted((person1, person2) -> person1.getName().compareTo(person2.getName()));
+
+    //分页
+    /*List<ReportMarketingCourseVo> result = resultTemp.entrySet().stream().map(obj -> obj.getValue()).
+            //sorted(Comparator.comparing(obj -> obj.getId(),Comparator.reverseOrder()));
+                    sorted((obja,objb)->objb.getId().compareTo(obja.getId())).
+                    skip((pageNo-1)*pageSize).limit(pageNo*pageSize).collect(Collectors.toList());
+            voPage.setRecords(result);
+            voPage.setTotal(size);//总页数
+            voPage.setSize(pageSize);//每页条数
+            voPage.setCurrent(pageNo);//当前页*/
 
     // 遍历List<Map<String,Integer>>集合的元素
     //list.stream().forEach(map -> map.forEach((k,v) -> System.out.println("key:value = " + k + ":" + v)));
